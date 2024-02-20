8137c9859c49db91f992ef7e8940402a515070d3
Rohan R <rohanr9084@gmail.com>
2024-02-20 09:03:24 +0530

    Added basic html layout and css

--- note-flow/src/App.tsx
+++ note-flow/src/App.tsx
@@ -3,7 +3,20 @@ import "./App.css";
 const App = () => {
   return (
     <div className="app-container">
-      
+      <form className="note-form">
+        <input placeholder="Title" type="text" required></input>
+        <textarea placeholder="Content" rows={10} required></textarea>
+        <button type="submit">Add Note</button>
+      </form>
+      <div className="notes-grid">
+        <div className="notes-item">
+          <div className="notes-header">
+            <button>X</button>
+          </div>
+          <h2>Note Title</h2>
+          <p>Note Content</p>
+        </div>
+      </div>
     </div>
   );
 };
